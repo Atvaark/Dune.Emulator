5811930ff8ad9ad77ca8de68255f27ab2ba55fb6
Atvaark <Atvaark@users.noreply.github.com>
2016-02-24 22:05:03 +0100

    Added support for decrypting packets

---
+++ Dune/Encryption/Blowfish/Pack.cs
@@ -0,0 +1,37 @@
+﻿#region License
+//The Bouncy Castle License
+//
+//Copyright (c) 2000 - 2015 The Legion of the Bouncy Castle Inc. (http://www.bouncycastle.org)
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+//to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+namespace Dune.Encryption.Blowfish
+{
+    internal static class Pack
+    {
+        internal static void UInt32_To_LE(uint n, byte[] bs, int off)
+        {
+            bs[off] = (byte)(n);
+            bs[off + 1] = (byte)(n >> 8);
+            bs[off + 2] = (byte)(n >> 16);
+            bs[off + 3] = (byte)(n >> 24);
+        }
+
+        internal static uint LE_To_UInt32(byte[] bs, int off)
+        {
+            return (uint)bs[off]
+                | (uint)bs[off + 1] << 8
+                | (uint)bs[off + 2] << 16
+                | (uint)bs[off + 3] << 24;
+        }
+    }
+}
---
+++ Dune/Encryption/Crypto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Dune.Encryption.Blowfish;
+
+namespace Dune.Encryption
+{
+    public static class Crypto
+    {
+        private const int BlockSize = BlowfishEngine.BLOCK_SIZE;
+
+        public static byte[] Decrypt(byte[] data, byte[] key)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            if (key == null) throw new ArgumentNullException("key");
+            return ProcessData(data, key, forEncryption: false);
+        }
+
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            if (key == null) throw new ArgumentNullException("key");
+            return ProcessData(data, key, forEncryption: true);
+        }
+
+        private static byte[] ProcessData(byte[] data, byte[] key, bool forEncryption)
+        {
+            byte[] input = data;
+
+            bool paddingRequired = data.Length % BlockSize > 0;
+            if (paddingRequired)
+            {
+                input = new byte[data.Length + BlockSize - data.Length % BlockSize];
+                Buffer.BlockCopy(data, 0, input, 0, data.Length);
+            }
+
+            byte[] output = new byte[input.Length];
+
+            BlowfishEngine engine = new BlowfishEngine();
+            engine.Init(forEncryption, key);
+
+            int offset = 0;
+            while (offset < input.Length)
+            {
+                engine.ProcessBlock(input, offset, output, offset);
+
+                offset += BlockSize;
+            }
+
+            if (paddingRequired)
+            {
+                byte[] unpaddedOutput = new byte[data.Length];
+                Buffer.BlockCopy(output, 0, unpaddedOutput, 0, unpaddedOutput.Length);
+                return unpaddedOutput;
+            }
+
+            return output;
+        }
+    }
+}
